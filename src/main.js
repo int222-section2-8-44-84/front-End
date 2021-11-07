b01abd2145dea62e64e4a86cbb33337f4b6aad9b
warisara <warisara.khrua@mail.kmutt.ac.th>
2021-11-07 21:19:39 +0700

    [fixed] git branch round 3

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import 'remixicon/fonts/remixicon.css'
 import BaseNav from './components/BaseNav'
 import Popup from './components/popup.vue'
 import BaseButton from './components/BaseButton.vue'
+import ConfirmDelete from './components/ConfirmDelete.vue'
+import ManageModal from './components/ManageModal'
 
 const app = createApp(App)
 app.use(router).mount('#app')
 app.component('base-nav', BaseNav)
 app.component('pop-up', Popup)
 app.component('base-button', BaseButton)
+app.component('confirm-delete',ConfirmDelete)
+app.component('edit-member',ManageModal)
